c70bb2529044eae5d4b4528dac13cde4be70b4db
Siyad Savad <siyadsavad313@gmail.com>
2025-08-15 01:30:19 +0530

    models for Driver, Route and Order is create. not migrated

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,7 +5,40 @@ from django.utils import timezone
 
 
 
+class Driver(models.Model):
+  name = models.CharField(max_length=100, unique=True)
+  shift_hours = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+  past_week_hours = models.JSONField(default=list)
+
+  def __str__(self):
+      return self.name
+  
+
+class Route(models.Model):
+   route_id = models.PositiveIntegerField(unique=True)
+   distance_km = models.PositiveIntegerField(validators=[MinValueValidator(0)])
+   TRAFFIC_CHOICES = (("Low","Low"),("Medium","Medium"),("High","High"))
+   traffic_level = models.CharField(max_length=10, choices=TRAFFIC_CHOICES)
+   base_time_min = models.PositiveIntegerField()
+
+   def __str__(self):
+       return f"Route {self.route_id}"
+   
+
+class Order(models.Model):
+   order_id = models.PositiveIntegerField(unique=True)
+   value_rs = models.PositiveIntegerField()
+   route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name="orders")
+   delivery_time = models.DateTimeField(null=True, blank=True)
+
+   def __str__(self):
+       return f"Order {self.order_id}"
+   
+
 
    
+
+
+
    
 
