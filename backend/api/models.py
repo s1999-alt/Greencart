0b58075fa5bf17e0b9be661cd7ae943ed6c991af
Siyad Savad <siyadsavad313@gmail.com>
2025-08-15 01:25:34 +0530

    initial commit + app created + basic configurations setted

---
+++ backend/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MinValueValidator
+from django.utils import timezone
+
+
+
+
+   
+   
+
