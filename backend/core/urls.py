09a57d92604124798274f706cdf5eac5e92e76b1
Siyad Savad <siyadsavad313@gmail.com>
2025-08-15 20:43:53 +0530

    changed

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,17 +7,17 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 
-router.register(r"drivers", DriverViewSet, basename="drivers")
-router.register(r"routes", RouteViewSet, basename="routes")
-router.register(r"orders", OrderViewSet, basename="orders")
-router.register(r"simulationresult", SimulationResultViewSet, basename="simulationresult")
+router.register(r"api/drivers", DriverViewSet, basename="drivers")
+router.register(r"api/routes", RouteViewSet, basename="routes")
+router.register(r"api/orders", OrderViewSet, basename="orders")
+router.register(r"api/simulationresult", SimulationResultViewSet, basename="simulationresult")
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("/", include(router.urls)),
 
-    path("simulate/", SimulationView.as_view()),
+    path("api/simulate/", SimulationView.as_view()),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
